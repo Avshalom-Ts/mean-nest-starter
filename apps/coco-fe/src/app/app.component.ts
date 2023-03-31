9d27962d8e7ee2e3ce5858839ee56f4735656837
Avner Hattab <avner.h@nanox.vision>
2023-03-31 14:51:08 +0300

    keycloack

--- apps/coco-fe/src/app/app.component.ts
+++ apps/coco-fe/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import { AuthModule } from '@coco/auth';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, AuthModule],
   selector: 'coco-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
